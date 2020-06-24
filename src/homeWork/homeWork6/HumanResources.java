7c764d398555405ee5a04bb906b968f62990f785
Stanislav Sheiko <sever.innopolis@gmail.com>
2020-06-24 16:24:32 +0300

    added 6 homework

---
+++ src/homeWork/homeWork6/HumanResources.java
@@ -0,0 +1,35 @@
+package homeWork.homeWork6;
+
+import java.util.ArrayList;
+
+abstract class HumanResources {
+    private String name;
+    private String surname;
+    private int age;
+
+    HumanResources(String name, String surname, int age) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+
+    public String getName() {return this.name; }
+
+    public void setName(String name) {this.name = name; }
+
+    public String getSurname() {
+        return this.surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
